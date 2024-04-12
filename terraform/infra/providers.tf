8ddd38be9b310f3a035dbc88a2f153009939e8d6
Pedro Marques da Silva <posilva@gmail.com>
2024-04-12 21:44:13 +0100

    chore: remove local stack configs to allow to deploy to AWS

--- terraform/infra/providers.tf
+++ terraform/infra/providers.tf
@@ -8,24 +8,24 @@ provider "aws" {
     }
   }
 
-  #--Mocked Credentials
-  access_key = "test"
-  secret_key = "test"
+  #   #--Mocked Credentials
+  #   access_key = "test"
+  #   secret_key = "test"
 
 
-  #--These settings allow for authentication and other validations which are enforced
-  #--in the AWS provider to be bypassed by Localstack.
-  skip_credentials_validation = true
-  skip_metadata_api_check     = true
-  skip_requesting_account_id  = true
+  #   #--These settings allow for authentication and other validations which are enforced
+  #   #--in the AWS provider to be bypassed by Localstack.
+  #   skip_credentials_validation = true
+  #   skip_metadata_api_check     = true
+  #   skip_requesting_account_id  = true
 
-  #--Redirect Service Endpoints to Localstack. Whilst we won't be any of these it's good
-  #--to see how they work and one should be specified to avoid rogue creations.
-  #--See the Localstack docs for a full list of suitable endpoints):
-  #--https://registry.terraform.io/providers/hashicorp/aws/latest/docs/guides/custom-service-endpoints#available-endpoint-customizations
-  endpoints {
-    dynamodb = "http://localhost:4566"
-  }
+  #   #--Redirect Service Endpoints to Localstack. Whilst we won't be any of these it's good
+  #   #--to see how they work and one should be specified to avoid rogue creations.
+  #   #--See the Localstack docs for a full list of suitable endpoints):
+  #   #--https://registry.terraform.io/providers/hashicorp/aws/latest/docs/guides/custom-service-endpoints#available-endpoint-customizations
+  #   endpoints {
+  #     dynamodb = "http://localhost:4566"
+  #   }
 }
 terraform {
   required_providers {
